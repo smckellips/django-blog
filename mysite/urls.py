fd5728ba896228ad46bf38ea6e59db36fc2f57e5
Sean McKellips <mckellip@adobe.com>
2021-04-25 20:37:46 -0700

    Add inline categories to posts

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import include, path
 
 urlpatterns = [
     path('', include('blogging.urls')),  
     path('polling/', include('polling.urls')),
     path('admin/', admin.site.urls),
+    path('login/', LoginView.as_view(template_name='login.html'), name="login"),
+    path('logout/', LogoutView.as_view(next_page='/'), name="logout"),
 ]
 
